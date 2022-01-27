1aa1b780428bb4299cc6c6d34d2394d67ef56f29
iwsarmad <96718403+iwsarmad@users.noreply.github.com>
2022-01-27 11:50:36 +0300

    xcvcxv
    cxvcxcv

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\io_art;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ApiController extends Controller
 {
@@ -14,5 +15,46 @@ class ApiController extends Controller
 
         $io_arts=io_art::all();
         return response(["io_art"=>$io_arts],200);
+    }
+
+    public function b2b_invites()
+    {
+
+        $b2b_invites=DB::select('call b2b_invites()');
+        return response(["b2b_invites"=>$b2b_invites],200);
+    }
+
+
+    public function customers()
+    {
+        $customers=DB::select('call customers()');
+
+        return response(["customers"=>$customers],200);
+    }
+
+    // Suppliers
+
+
+    public function suppliers()
+    {
+        $suppliers=DB::select('call suppliers()');
+
+        return response(["suppliers"=>$suppliers],200);
+    }
+
+
+    public function customer_details(Request $request)
+    {
+        $customer_details=DB::select('call customer_details('.$request->COD_CLIFOR.')');
+
+        return response(["customer_details"=>$customer_details],200);
+    }
+
+
+    public function customers_contact(Request $request)
+    {
+        $customers_contact=DB::select('call customers_contact('.$request->COD_CLIFOR.')');
+
+        return response(["customers_contact"=>$customers_contact],200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::get('/GetxData', [App\Http\Controllers\ApiController::class, 'GetxData'])->name('GetxData');
+Route::get('/b2b_invites', [App\Http\Controllers\ApiController::class, 'b2b_invites'])->name('b2b_invites');
+Route::get('/customers', [App\Http\Controllers\ApiController::class, 'customers'])->name('customers');
+Route::get('/suppliers', [App\Http\Controllers\ApiController::class, 'suppliers'])->name('suppliers');
+Route::get('/customer_details', [App\Http\Controllers\ApiController::class, 'customer_details'])->name('customer_details');
+Route::get('/customers_contact', [App\Http\Controllers\ApiController::class, 'customers_contact'])->name('customers_contact');
